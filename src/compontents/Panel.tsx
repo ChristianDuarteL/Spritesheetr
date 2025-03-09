654e21e75d5cbbbff85afa13daf655bdbd22dc83
Christian Duarte <w.christian.duarte@outlook.com>
2025-03-09 11:29:41 -0700

    Changed border colors and scrollbar colors

--- src/compontents/Panel.tsx
+++ src/compontents/Panel.tsx
@@ -8,11 +8,11 @@ export interface PanelProps {
 }
 
 export default function Panel(props: PropsWithChildren<PanelProps>) {
-    return <section className='noisy-background flex flex-col border border-background-950/25 rounded-lg h-full'>
-        <header className={`py-2 px-2 flex items-center ${!props.collapsed ? "border-b border-background-950/25" : "flex-1"}`}>
+    return <section className='noisy-background flex flex-col border border-background-300 rounded-lg h-full overflow-hidden'>
+        <header className={`py-2 px-2 flex items-center ${!props.collapsed ? "border-b border-background-300" : "flex-1"}`}>
             {props.title}
         </header>
-        <div className={cx("flex-1 flex flex-col p-2 px-2", !props.collapsed ? "overflow-auto" : "hidden", props.className)}>
+        <div className={cx("flex-1 flex flex-col p-2 px-2 scrollbar-bg-background-100 scrollbar-fg-background-300 scrollbar", !props.collapsed ? "overflow-auto" : "hidden", props.className)}>
             {props.children}
         </div>
     </section>;
