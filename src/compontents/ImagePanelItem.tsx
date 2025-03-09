2836686176e4174299f862ca4a53539cb51a853d
Christian Duarte <w.christian.duarte@outlook.com>
2025-03-09 11:28:41 -0700

    Fixed image stretching

--- src/compontents/ImagePanelItem.tsx
+++ src/compontents/ImagePanelItem.tsx
@@ -13,7 +13,7 @@ function _ImagePanelItem(props: ImagePanelItemProps) {
     return (
         <div className="group p-2 rounded-md bg-accent-200 shadow-md flex flex-col gap-2 relative">
             <div className="aspect-square rounded-sm overflow-hidden bg-accent-50">
-                <img className="w-full h-full" src={props.image.url}/>
+                <img className="w-full h-full object-contain" src={props.image.url}/>
             </div>
             <input className="hover:bg-accent-300/50 focus:bg-accent-300 rounded-sm p-1 text-sm outline-0" type="text" value={props.image.name} onChange={(element: ChangeEvent) => props.setName(props.id, (element.target as HTMLInputElement).value)}/>
             <div className="opacity-0 transform translate-y-2 group-hover:translate-y-0 group-hover:opacity-100 transition absolute -right-2 -top-2 rounded-full shadow-sm">
