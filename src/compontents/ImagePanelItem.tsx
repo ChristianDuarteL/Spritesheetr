77ca9b0098def18841ff67afacfbb90469bca3f5
Christian Duarte <w.christian.duarte@outlook.com>
2025-03-12 19:46:32 -0700

    Render images pixelated and show the button on focus

--- src/compontents/ImagePanelItem.tsx
+++ src/compontents/ImagePanelItem.tsx
@@ -13,10 +13,10 @@ function _ImagePanelItem(props: ImagePanelItemProps) {
     return (
         <div className="group p-2 rounded-md bg-accent-200 shadow-md flex flex-col gap-2 relative">
             <div className="aspect-square rounded-sm overflow-hidden bg-accent-50">
-                <img className="w-full h-full object-contain" src={props.image.url}/>
+                <img className="w-full h-full object-contain render-pixelated" src={props.image.url}/>
             </div>
             <input className="hover:bg-accent-300/50 focus:bg-accent-300 rounded-sm p-1 text-sm outline-0" type="text" value={props.image.name} onChange={(element: ChangeEvent) => props.setName(props.id, (element.target as HTMLInputElement).value)}/>
-            <div className="opacity-0 transform translate-y-2 group-hover:translate-y-0 group-hover:opacity-100 transition absolute -right-2 -top-2 rounded-full shadow-sm">
+            <div className="opacity-0 transform translate-y-2 group-hover:translate-y-0 group-hover:opacity-100 focus-within:translate-y-0 focus-within:opacity-100 transition absolute -right-2 -top-2 rounded-full shadow-sm">
                 <button className="bg-accent-50 p-1 rounded-full shadow-sm cursor-pointer" onClick={() => props.delete(props.id)}>
                     <XIcon></XIcon>
                 </button>
