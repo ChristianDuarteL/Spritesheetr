1117d3c2c26f3a811d289045a1f4b648a3bdb871
Christian Duarte <w.christian.duarte@outlook.com>
2025-03-12 19:47:13 -0700

    Show card to add more images

--- src/compontents/ImagesPanel.tsx
+++ src/compontents/ImagesPanel.tsx
@@ -3,7 +3,7 @@ import ResizablePanel from "./ResizablePanel";
 import { useImages } from "../providers/images";
 import { useCallback } from "react";
 import {useDropzone} from 'react-dropzone'
-import { Upload } from "lucide-react";
+import { PlusCircle, Upload } from "lucide-react";
 import { makeFromFile } from "../lib/images";
 import Button from "./Button";
 
@@ -30,6 +30,10 @@ export default function ImagesPanel() {
             <div className="w-full min-h-full relative" {...getRootProps()}>
                 <div className="w-full grid grid-cols-3 gap-4 p-4">
                     {Object.entries(images).map(([i, e]) => <ImagePanelItem key={i} id={i} image={e} setName={renameImage} delete={removeImage}/>)}
+                    {hasImages ? <button onClick={open} className="p-2 rounded-md bg-accent-100 focus:bg-accent-50 focus:text-accent-400/80 active:text-accent-200 active:bg-accent-50/95 transition-colors shadow-md flex flex-col items-center justify-center text-accent-400 cursor-pointer">
+                        <PlusCircle className="h-10 w-10"/>
+                        <span className="uppercase text-sm font-semibold">Add more</span> 
+                    </button> : null}
                 </div>
                 <input {...getInputProps()} className="hidden"/>
                 { 
